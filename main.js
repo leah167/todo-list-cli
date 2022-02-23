41db5388201b03872ff46143e94d7b7292006f3d
Leah Sanchez <leahsanchez@Leahs-MacBook-Pro.local>
2022-02-22 23:55:35 -0700

    to-do list now meets all requirements, added if statements for selctions 1 and 2

--- main.js
+++ main.js
@@ -2,9 +2,19 @@ const prompt = require('prompt-sync')({sigint: true});
 
 let toDoList = [];
 
+let completed = [];
+
+let toAdd = [];
+
+let toComplete = [];
+
+let status = [];
+
 let count = 0
 
 let selection = 0
+
+// array[i] = "new value";
 
 console.log("~ To-Do List App ~");
 
@@ -12,11 +22,14 @@ console.log("*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*");
 
 while (selection !== 3) {
 
+    
     if (toDoList.length <= 0) {
 
         console.log("You have no items in your to-do list.");
 
     }
+
+    
 
     console.log("*~ Make a selection ~*");
 
@@ -39,10 +52,92 @@ while (selection !== 3) {
         console.log("*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*");
     }
 
+    if (selection === 1) {
+        console.log("*~ Add an item to your to-do list ~*");
+
+        console.log("What is the title of the item you wish to add?");
+
+        let toAdd = prompt(">");
+
+        toDoList.push(toAdd);
+
+        // console.log(toDoList)
+
+        console.log("*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*");
+
+        console.log("You have " + Number(toDoList.length) + " item(s) to do.");
+
+        for (let count = 0; count < toDoList.length; count++) {
+            
+            completed[count] = false
+
+            if (completed[count] === false) {
+
+                status[count] = "[incomplete]"
+
+            } else if (completed[count] === true) {
+                
+                status[count] = "[complete]"
+            }
+
+
+            console.log(Number(count + 1) + "." + status[count] + toDoList[count]);
+
+            console.log("*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*");
+            
+            
+        }
+        
+
+
+    } 
+    if (selection === 2) {
+
+        console.log("*~ Mark a to-do item as completed! ~*");
+
+        console.log("Which to-do item would you like to mark as completed?");
+
+        let toComplete = Number(prompt(">"));
+
+        console.log("*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*");
+
+        console.log("You have " + Number(toDoList.length) + " item(s) to do.");
+
+
+
+        for (let count = 0; count < toDoList.length; count++) {
+
+            
+
+            // let toDo = toDoList[toComplete-1];
+
+            completed[toComplete-1] = true
+
+            // console.log(toDo);
+
+            // let isComplete = completed[count];
+
+            if (completed[count] === false) {
+
+                status[count] = "[incomplete]"
+
+            } else if (completed[count] === true) {
+                
+                status[count] = "[complete]"
+            }
+
+            console.log(Number(count + 1) + "." + [status[count]] + toDoList[count]);
+            
+        }
+    }
     if (selection === 3) {
 
         return console.log("Goodbye!");
         
     } 
+  
+
     
+
+
 }
